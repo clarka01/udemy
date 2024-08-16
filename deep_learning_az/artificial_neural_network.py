7f148e23d262e62e2dfbd99084ecd9e825d5cd28
clarka <aaron.clark@hamiltonbeach.com>
2024-08-16 12:22:04 -0400

    minimal

--- deep_learning_az/artificial_neural_network.py
+++ deep_learning_az/artificial_neural_network.py
@@ -12,7 +12,8 @@ tf.__version__
 
 # Part 1 - Data Preprocessing
 
-path = r'G:\My Drive\DATA_SCIENCE\deep_learning\Part 1 - Artificial Neural Networks'
+# path = r'G:\My Drive\DATA_SCIENCE\deep_learning\Part 1 - Artificial Neural Networks'
+path = r'C:\temp\local_learning'
 fl = os.path.join(path, 'Churn_Modelling.csv')
 # Importing the dataset
 dataset = pd.read_csv(fl)
@@ -62,7 +63,7 @@ print(X)
 #%%
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 17325)
 
 
 
@@ -72,9 +73,6 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-
-
-
 
 
 
